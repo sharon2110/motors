7d981fadb4247c57083f7b871fe7628de3cc1691
sharon2110 <105511530+sharon2110@users.noreply.github.com>
2022-05-15 14:42:21 -0600

    Update conexion.php

--- scripts/conexion.php
+++ scripts/conexion.php
@@ -2,7 +2,7 @@
 
 $config = parse_ini_file('../conf.ini');
 try{
-   $con = new PDO('pgsql:host=localhost;port=5432;dbname='.$config['db'],$user=$config['username'],$password=$config['password']);
+   $con = new PDO('pgsql:host=ec2-52-200-215-149.compute-1.amazonaws.com;port=5432;dbname='.$config['db'],$user=$config['username'],$password=$config['password']);
    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    
 }catch(PDOException $e){
